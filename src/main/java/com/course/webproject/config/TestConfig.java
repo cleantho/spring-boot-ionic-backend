062bd4db82ae6bb3ca630f1cbbb7daf91171bd50
cleantho <cleantho@gmail.com>
2023-03-12 18:57:38 -0300

    E-mail HTML

--- src/main/java/com/course/webproject/config/TestConfig.java
+++ src/main/java/com/course/webproject/config/TestConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.course.webproject.services.EmailService;
 import com.course.webproject.services.MockEmailService;
@@ -24,6 +26,11 @@ public class TestConfig {
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender javaMailSender() {
+		return new JavaMailSenderImpl();
 	}
 	
 }
